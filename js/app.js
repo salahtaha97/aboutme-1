352c0de7e4626b02c165d5fe1113f9d762bc5f7b
FerasAljayyousi <feras.aljayyousi@gmail.com>
2021-06-09 15:57:57 +0300

    edit file

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 'use strict'
 
-let userName = prompt('whats your name ?');
-alert("welcome" +userName);
+//let userName = prompt('whats your name ?');
+//alert("welcome" +userName);
 
-let pizzaFlav = prompt('do i like pizza with painapple ?').toLowerCase();
+//let pizzaFlav = prompt('do i like pizza with painapple ?').toLowerCase();
 //YES 
 //Yes yes yEs yeS
 
-console.log(pizzaFlav);
+//console.log(pizzaFlav);
 
  //pizzaFlav = pizzaFlav.toLowerCase();
 
@@ -16,61 +16,73 @@ console.log(pizzaFlav);
 
 
 
-if(pizzaFlav === 'yes' || pizzaFlav === 'y') {
-    alert("false never like it ");
-  } else if (pizzaFlav === 'no' || pizzaFlav === 'n') {
-    alert("true");
-  } else {
-    alert("Pease type (yes) or (no) or (y) or (n)");
-  }
+//if(pizzaFlav === 'yes' || pizzaFlav === 'y') {
+    //alert("false never like it ");
+  //} else if (pizzaFlav === 'no' || pizzaFlav === 'n') {
+    //alert("true");
+  //} else {
+    //alert("Pease type (yes) or (no) or (y) or (n)");
+  //}
 
 
-  let gaming = prompt('do i like gaming?');
+  //let gaming = prompt('do i like gaming?');
   
-  console.log(gaming);
+  //console.log(gaming);
 
   
-if(gaming === 'yes' || gaming === 'y') {
-  alert("sure i do ");
-} else if (gaming === 'no' || gaming === 'n') {
-  alert("false");
-} else {
-  alert("Pease type (yes) or (no) or (y) or (n)");
-}
+//if(gaming === 'yes' || gaming === 'y') {
+  //alert("sure i do ");
+//} //else if (gaming === 'no' || gaming === 'n') {
+  //alert("false");
+//} //else {
+  //alert("Pease type (yes) or (no) or (y) or (n)");
+//}
 
-let rage = prompt('do i rage so fast ?');
-console.log(rage);
-
-
-if(rage === 'yes' ||rage === 'y') {
-  alert("true a little bit ");
-} else if (rage === 'no' || rage=== 'n') {
-  alert("not right");
-} else {
-  alert("Pease type (yes) or (no) or (y) or (n)");
-}
-
-let city = prompt('am i from zarqa city?');
-console.log(city);
+//let rage = prompt('do i rage so fast ?');
+//console.log(rage);
 
 
-if(city === 'yes' || city === 'y') {
-  alert("true ");
-} else if (city === 'no' || city === 'n') {
-  alert("false");
-} else {
-  alert("Pease type (yes) or (no) or (y) or (n)");
-}
+//if(rage === 'yes' ||rage === 'y') {
+  //alert("true a little bit ");
+//} else if (rage === 'no' || rage=== 'n') {
+  //alert("not right");
+//} else {
+  //alert("Pease type (yes) or (no) or (y) or (n)");
+//}
 
-let heights = prompt('do you think iam afraid from heights?');
-console.log(heights);
+//let city = prompt('am i from zarqa city?');
+//console.log(city);
 
 
-if(heights === 'yes' || heights === 'y') {
-  alert("true ");
-} else if (heights === 'no' || heights === 'n') {
-  alert("false");
-} else {
-  alert("Pease type (yes) or (no) or (y) or (n)");
-}
+//if(city === 'yes' || city === 'y') {
+  //alert("true ");
+//} else if (city === 'no' || city === 'n') {
+  //alert("false");
+//} else {
+  //alert("Pease type (yes) or (no) or (y) or (n)");
+//}
+
+//let heights = prompt('do you think iam afraid from heights?');
+//console.log(heights);
+
+
+//if(heights === 'yes' || heights === 'y') {
+  //alert("true ");
+//} else if (heights === 'no' || heights === 'n') {
+  //alert("false");
+//} else {
+  //alert("Pease type (yes) or (no) or (y) or (n)");
+//}
+
+let guess = 7;
+let number = 3;
+//do{
+  let answer = prompt('can you guess the number from 1 to 10?');
+//number++;
+
+for (let i = 0;i<number;i++){
+  if(answer > 7){alert("far");}else if(answer <7){alert("close");}else if(answer == 7){alert ("corect you guess it");break}
+  answer = prompt('can you guess the number from 1 to 10?');
+};
+
 
